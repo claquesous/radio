fc52de2a00f41d78738cd2885e21bd01e9c9d33d
Geoff Crew <gc97@cornell.edu>
2024-02-16 11:42:53 -0500

    call curl directly instead of via library

--- ClaqRadio/Ices.pm
+++ ClaqRadio/Ices.pm
@@ -1,7 +1,6 @@
 package ClaqRadio::Ices;
 
 use URI;
-use WWW::Curl::Easy;
 use JSON 'decode_json';
 
 use ClaqRadio::Config;
@@ -12,21 +11,9 @@ our @EXPORT_OK = qw(ices_init ices_get_next);
 
 sub get_next(){
     my $uri = URI->new( ClaqRadio::Config->host . "plays.json"  );
-    my $curl = WWW::Curl::Easy->new;
-    my $response_body;
+    my $curlcmd = `curl -s -X POST $uri`;
 
-    $curl->setopt(CURLOPT_URL, $uri);
-    $curl->setopt(CURLOPT_POST, 1);
-    $curl->setopt(CURLOPT_WRITEDATA,\$response_body);
-
-    my $retcode = $curl->perform;
-    my $rspcode = $curl->getinfo(CURLINFO_HTTP_CODE);
-
-    if ($retcode != 0 || $rspcode < 200 || $rspcode>=400){
-	return "";
-    }
-
-    return $response_body;
+    return $curlcmd;
 }
 
 sub ices_init{
