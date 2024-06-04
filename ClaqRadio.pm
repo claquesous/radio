8fd6e0bea667498ed5713698beaf180e3b7f3036
Geoff Crew <gc97@cornell.edu>
2024-06-04 13:19:46 -0400

    return json (without filepath) as metadata

--- ClaqRadio.pm
+++ ClaqRadio.pm
@@ -1,19 +1,17 @@
 #!/usr/bin/perl -w
 
-use URI;
-use JSON 'decode_json';
+use JSON;
 
 my $metadata;
 
 sub ices_get_next {
-    my $uri = URI->new( $ENV{CLAQRADIO_STREAM_URL} . "/plays.json"  );
-    my $json_result = `curl -s -X POST $uri`;
+    my $url = $ENV{CLAQRADIO_STREAM_URL} . "/plays.json";
+    my $json_result = `curl -s -X POST $url`;
     if ( $json_result ){
       my %json = %{decode_json($json_result)};
-      my %song = %{$json{'song'}};
-      my %artist = %{$json{'artist'}};
-      $metadata = $artist{'name'} . " - " . $song{'title'};
-      return $song{'path'};
+      my $path = delete $json{'song'}{'path'};
+      $metadata = encode_json(\%json);
+      return $path;
     }
     return "";
 }
