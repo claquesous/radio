3f3745f30f00941d0c1008e21854dc22e6cc98a3
Geoff Crew <gc97@cornell.edu>
2024-06-04 01:12:54 -0400

    simplify perl module and support metadata

--- ClaqRadio.pm
+++ ClaqRadio.pm
@@ -1,9 +1,26 @@
 #!/usr/bin/perl -w
 
-use ClaqRadio::Ices;
+use URI;
+use JSON 'decode_json';
 
-sub ices_get_next() {
-    return ClaqRadio::Ices->ices_get_next();
+my $metadata;
+
+sub ices_get_next {
+    my $uri = URI->new( $ENV{CLAQRADIO_STREAM_URL} . "/plays.json"  );
+    my $json_result = `curl -s -X POST $uri`;
+    if ( $json_result ){
+      my %json = %{decode_json($json_result)};
+      my %song = %{$json{'song'}};
+      my %artist = %{$json{'artist'}};
+      $metadata = $artist{'name'} . " - " . $song{'title'};
+      return $song{'path'};
+    }
+    return "";
 }
 
-371;
+sub ices_get_metadata {
+    return $metadata;
+}
+
+return 1;
+
